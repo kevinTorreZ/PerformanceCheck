37a80ec7bfad309250a31f8f0cd273bedc4f9c62
MatLumber <89222794+MatLumber@users.noreply.github.com>
2023-09-26 17:20:56 -0300

    NOTIFICACIONES JEJEJE

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -2,14 +2,17 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { useNavigate, Link } from 'react-router-dom';
-import {useAuth} from '../components/verificador'
+import { useAuth } from '../components/verificador'
+import { ToastContainer, toast } from 'react-toastify';
+import { Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function Login() {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const navigate = useNavigate();
     const { login } = useAuth();
 
-    useEffect(() => {   
+    useEffect(() => {
         const token = localStorage.getItem('token');
         if (token) {
             navigate('/');
@@ -17,31 +20,44 @@ export function Login() {
     }, [navigate]);
 
     const onSubmit = async data => {
-        try{
+        try {
             const response = await axios.post('http://localhost:8000/api/token/', data);
             const tokens = response.data;
             login(tokens.access, tokens.refresh, tokens.user);
             localStorage.setItem('refreshToken', tokens.refresh);
             navigate('/');
-        }catch(error){
-            document.getElementById('ErrorLogin').innerHTML = '<p>Contraseña o correo inválido!</p>'
+        } catch (error) {
+            toast.error('Contraseña o correo inválido!');
         }
     };
 
     return (
         <div className='loginDiv'>
-        <script src="https://kit.fontawesome.com/c1366bcc7f.js" crossOrigin="anonymous"></script>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
-                <form onSubmit={handleSubmit(onSubmit)} id='formularioLogin'>
-                    <h1>Iniciar sesión</h1>
-                    <input type="email" {...register('email', { required: 'El correo es requerido' })} style={{fontFamily: '"Cabin", sans-serif, FontAwesome'}} placeholder='&#xF0e0; Correo' onInput={() => document.getElementById('ErrorLogin').innerHTML = ''} />
-                    {errors.email?.message && <p>{errors.email.message}</p>}
-                    <input type="password" {...register('password', { required: 'La contraseña es requerida' })} style={{fontFamily: '"Cabin", sans-serif, FontAwesome'}} placeholder='&#xF023; Contraseña' onInput={() => document.getElementById('ErrorLogin').innerHTML = ''} />
-                    {errors.password?.message && <p>{errors.password.message}</p>}
-                    <button type="submit">Iniciar sesión</button>
-                    <div id='ErrorLogin'>
-                    </div>
-                </form>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                limit={1}
+                transition={Slide}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable
+                pauseOnHover
+                theme="dark"
+                style={{marginTop:"4rem"}}
+            />
+            <script src="https://kit.fontawesome.com/c1366bcc7f.js" crossOrigin="anonymous"></script>
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+            <form onSubmit={handleSubmit(onSubmit)} id='formularioLogin'>
+                <h1>Iniciar sesión</h1>
+                <input type="email" {...register('email', { required: 'El correo es requerido' })} style={{ fontFamily: '"Cabin", sans-serif, FontAwesome' }} placeholder='&#xF0e0; Correo' onInput={() => document.getElementById('ErrorLogin').innerHTML = ''} />
+                {errors.email?.message && <p>{errors.email.message}</p>}
+                <input type="password" {...register('password', { required: 'La contraseña es requerida' })} style={{ fontFamily: '"Cabin", sans-serif, FontAwesome' }} placeholder='&#xF023; Contraseña' onInput={() => document.getElementById('ErrorLogin').innerHTML = ''} />
+                {errors.password?.message && <p>{errors.password.message}</p>}
+                <button type="submit">Iniciar sesión</button>
+            </form>
         </div>
     );
 }
