799db8f8fde490cb300b009d715dc9b0ec19925a
MatLumber <89222794+MatLumber@users.noreply.github.com>
2023-09-25 22:59:09 -0300

    404
    e

--- Client/src/pages/NotFoundPage.jsx
+++ Client/src/pages/NotFoundPage.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 export function NotFoundPage() {
     return (
-      <div>
-            <p>Esta página no existe</p>
+      <div className='divMain404'>
+            <h1 style={{color: "black"}}>Under Construction</h1>
+            <div id="logoDunder404"></div>
+            <div id="stickMan"></div>
+            <h1><span style={{color: "black"}}>Coming</span> C<span>h</span>r<span>i</span>s<span>t</span>m<span>a</span>s <span>2</span>0<span>0</span>2<span>!</span></h1>
       </div>
     );
 }
